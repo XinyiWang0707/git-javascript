1e6855b83570b0bf993b0dd903fd6baaa99797e5
Xinyi Wang <wang0707@algonquinlive.com>
2020-01-13 15:32:57 -0500

    Adds index.html and script.js files

---
+++ script.js
@@ -0,0 +1,13 @@
+let name = 'Xinyi Wang'
+console.log(`Hello, my name is ${name}.`)
+
+let number = 1
+console.log(`The number ${number} multiplied by 2 is ${number * 2}.`)
+
+// console.log(number * 2 * 5 / number - 7)
+let result = number * 2
+console.log(`The number multiplied by 5 is ${result * 5}.`)
+let nextResult = result * 5
+console.log(nextResult / number)
+let variable = nextResult / number
+console.log(variable - 7)
